f15861be49816803b5faea20dff9f8073c201d50
OlegGasul <oleg.gasul@gmail.com>
2021-01-25 00:24:42 +0100

    Tests

--- src/test/java/com/kuehnenagel/trackprocessor/service/TrackProcessorTest.java
+++ src/test/java/com/kuehnenagel/trackprocessor/service/TrackProcessorTest.java
@@ -1,22 +1,38 @@
 package com.kuehnenagel.trackprocessor.service;
 
+import com.kuehnenagel.trackprocessor.model.Coordinate;
+import com.kuehnenagel.trackprocessor.model.Track;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import java.io.File;
+import java.util.List;
 
+@SpringBootTest
+@RunWith(SpringJUnit4ClassRunner.class)
 class TrackProcessorTest {
 
     @Autowired
-    private CsvReader csvReader;
+    private GeoTools geoTools;
+
+    @Autowired
+    private TrackProcessor trackProcessor;
 
     @Test
     void shouldFindMostRepresentativeTrack() {
         // TODO implement
     }
 
-    private File getFile(String filename) {
-        ClassLoader classLoader = getClass().getClassLoader();
-        return new File(classLoader.getResource(filename).getFile());
+    private Track createTrack(Long fromSeq, Long toSeq, List<Coordinate> coordinates) {
+        return Track.builder()
+            .vesselId("ID")
+            .fromSeq(fromSeq)
+            .toSeq(toSeq)
+            .fromPort("A")
+            .toPort("B")
+            .coordinates(coordinates)
+            .build();
     }
 }
